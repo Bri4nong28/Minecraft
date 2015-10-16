0950a43e149af72ae9a9e82b45507682ebc16d09
Bri4nong28 <bri4nong28@gmail.com>
2015-10-16 15:00:38 +0700

    Create SuperSword Mod.js

---
+++ SuperSword Mod.js
@@ -0,0 +1,16 @@
+SuperSword Mod v 0.1.0 for MCPE 0.12.1
+Created by Brian
+Do not copy, Copying are illegal
+*/
+
+function newLevel(); {
+    clientMessage("Welcome")
+}
+
+function ModPE.setItem(948,"sword,4",   ,Super Sword)
+
+function Item.addCraftRecipe(948, 1, 1000, [278,9]
+
+function leaveGame() {
+    print("Goodbye");
+}
